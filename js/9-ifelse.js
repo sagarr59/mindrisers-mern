1da0f840a053d93d8379d69bdc2404d41b63332f
Sagar Raut <sagarraut2059@gmail.com>
2024-06-21 15:50:47 +0545

    nested ifelse

--- js/9-ifelse.js
+++ js/9-ifelse.js
@@ -1,31 +1,23 @@
-let user = {
+/* let user = {
   name: "Ram",
   paidStatus: true,
   hasScholorship: false,
   gender: "male",
 };
 
-// if (user.paidStatus && user.hasScholorship) {
-//   console.log(`${user.name} can give exam`);
-// } else  {
-//   console.log(`${user.name} cannot give exam`);
-// }
+if (user.paidStatus && user.hasScholorship) {
+  console.log(`${user.name} can give exam`);
+} else  {
+  console.log(`${user.name} cannot give exam`);
+}
 
-// if (user.paidStatus) {
-//   console.log(`${user.name} can give exam`);
-// } else if (user.hasScholorship) {
-//   console.log(`${user.name} can give exam`);
-// } else {
-//   console.log(`${user.name} cannot give exam`);
-// }
-
-if (user.gender == "male") {
-  console.log(`${user.name} can give the exam`);
-} else if (user.paidStatus || user.hasScholorship) {
-  console.log(`${user.name} can give the exam`);
+if (user.paidStatus) {
+  console.log(`${user.name} can give exam`);
+} else if (user.hasScholorship) {
+  console.log(`${user.name} can give exam`);
 } else {
-  console.log(`${user.name} cannot give the exam`);
-}  // add Mr. Ram or Mrs.Sita
+  console.log(`${user.name} cannot give exam`);
+} */
 
 /* operator 
 arithmetic operator
@@ -41,9 +33,24 @@ comparison operator
 console.log(10 > 10);
 console.log("Sagar " == "Sagar");
 
-//  ++count 
-//  count++ 
+/* ++count 
+ count++ 
 let number=100
 double(++100)
 let input =100
 double(input++)
+ */
+let user = {
+  name: "Ram",
+  paidStatus: true,
+  hasScholorship: false,
+  gender: "male",
+};
+
+if (user.gender == "male") {
+  console.log(`Mr. ${user.name} can give the exam`);
+} else if (user.paidStatus || user.hasScholorship) {
+  console.log(`Mr. ${user.name} can give the exam`);
+} else {
+  console.log(`Mr. ${user.name} cannot give the exam`);
+}
