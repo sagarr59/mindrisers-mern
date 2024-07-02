4da758daf240ddec1f8b1a68624e30d2979f1114
Sagar Raut <sagarraut2059@gmail.com>
2024-07-02 14:58:19 +0545

    updated

--- node_es_module/product.js
+++ node_es_module/product.js
@@ -1,11 +1,31 @@
-function editProducts() {
-  console.log("Edit Product");
-}
-function deleteProducts() {
-  console.log("delete Product");
+export const getProducts = () => {
+  console.log("getProducts");
+};
+
+export function editProduct() {
+  console.log("editProduct");
 }
 
-module.exports = {
-  editProducts: editProducts,
-  deleteProducts,
+export function deleteProduct() {
+  console.log("deleteProduct");
+}
+
+/*
+    module.exports.getProducts = getProducts
+    module.exports.editProduct = editProduct
+    module.exports.deleteProduct = deleteProduct
+ */
+
+// module.exports = {
+//   custom: getProducts,
+//   getProducts: getProducts,
+//   editProduct: editProduct,
+//   deleteProduct,
+// }
+
+export default {
+  custom: getProducts,
+  getProducts: getProducts,
+  editProduct: editProduct,
+  deleteProduct,
 };
