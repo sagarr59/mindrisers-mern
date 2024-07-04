0b1fa9279124e6723b05e7ada3de0772c372246f
Sagar Raut <sagarraut2059@gmail.com>
2024-07-04 23:45:32 +0545

    props used

--- react/src/components/Product.jsx
+++ react/src/components/Product.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-export default function Product({ title, price, discountedPrice, description }) {
+export default function Product(props) {
   return (
-    <div className="product">
-      <p className="product-title">{title}</p>
-      <p className="product-price">
-        Price: <span className="oldPrice">Rs.{price}</span> Rs.{discountedPrice}
-      </p>
-      <p className="product-description">{description}</p>
+    <div className="mainContainer">
+      <div className="product">
+        <p className="product-title">{props.title}</p>
+        <p className="product-price">
+          Price: <span className="oldPrice">Rs.{props.price}</span> Rs.
+          {props.discountedPrice}
+        </p>
+        <p className="product-description">{props.description}</p>
+      </div>
     </div>
   );
 }
